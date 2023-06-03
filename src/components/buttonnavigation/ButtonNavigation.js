236fa746258307d8f228d895d26a18bde0a42787
Anja <anja.labak12345@gmail.com>
2023-06-03 19:46:23 +0200

    Carousel and navigation fix

--- src/components/buttonnavigation/ButtonNavigation.js
+++ src/components/buttonnavigation/ButtonNavigation.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 import './ButtonNavigation.css';
+import {HiOutlineArrowNarrowDown} from 'react-icons/hi'
 
-const ButtonNavigation= () => {
+const ButtonNavigation = () => {
     return (
-        <Container className="navigation-btn pt-5 pb-5">
-            <Row>
-                <Col lg={12} className="navigation-btn mt-5 px-5 pt-5">
-                    <div className="btn-navigation__container">
-                        <button className="carousel__btn--round" data-text="Extraction">
-                        </button>
-                        <div className="carousel__horizontal-line"></div>
-                        <button className="carousel__btn--round" data-text="Transport">
-                        </button>
-                        <div className="carousel__horizontal-line"></div>
-                        <button className="carousel__btn--round" data-text="Refining">
-                        </button>
-                        <div className="carousel__horizontal-line"></div>
-                        <button className="carousel__btn--round" data-text="Export">
-                        </button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <>
+            <HiOutlineArrowNarrowDown className="navigation__arrow-icon" style={{ transform: "scaleX(2) scaleY(3)" }} />
+            <Container className="navigation-btn pt-5 pb-5">
+                <Row>
+                    <Col lg={12} className="navigation-btn mt-5 px-5 pt-5">
+                        <div className="btn-navigation__container">
+                            <button className="carousel__btn--round" data-text="Extraction">
+                            </button>
+                            <div className="carousel__horizontal-line"></div>
+                            <button className="carousel__btn--round" data-text="Transport">
+                            </button>
+                            <div className="carousel__horizontal-line"></div>
+                            <button className="carousel__btn--round" data-text="Refining">
+                            </button>
+                            <div className="carousel__horizontal-line"></div>
+                            <button className="carousel__btn--round" data-text="Export">
+                            </button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 };
 
