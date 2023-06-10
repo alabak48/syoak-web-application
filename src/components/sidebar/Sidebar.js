6e7e5a121871bd2aa682912f9775545fbd4d2364
Anja Labak <anja.labak12345@gmail.com>
2023-06-10 16:16:12 +0200

    Sidebar adaptation for mobile screen

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,38 +3,27 @@ import Document from "../../assets/Document.svg"
 import Graph from "../../assets/Graph.svg"
 import Admin from "../../assets/Admin.svg"
 import Logo from "../../assets/Logo.svg"
-import "./Sidebar.css"
+import "../../styles/styles.css"
 
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__item sidebar__item--logo">
-                <a>
-                    <img src={Logo} alt="Logo"/>
-                </a>
+                <img src={Logo} alt="Logo"/>
             </div>
             <div className="sidebar__item sidebar__item--one">
-                <a>
-                    <img src={Clock} alt="Clock"/>
-                </a>
+                <img src={Clock} alt="Clock"/>
             </div>
             <div className="sidebar__item sidebar__item--two">
-                <a>
-                    <img src={Document} alt="Document" />
-                </a>
+                <img src={Document} alt="Document"/>
             </div>
             <div className="sidebar__item sidebar__item--three">
-                <a>
-                    <img src={Graph} alt="Graph"/>
-                </a>
+                <img src={Graph} alt="Graph"/>
             </div>
             <div className="sidebar__item sidebar__item--four">
-                <a>
-                    <img src={Admin} alt="Admin"/>
-                </a>
+                <img src={Admin} alt="Admin"/>
             </div>
-
         </div>
     )
 }
